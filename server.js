126d8a1b58fee1fd454e6058210fd2f27f9410d3
eldanii13 <luis.cantilloooo13@gmail.com>
2024-10-29 21:55:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
+// const express = require('express');
+// const connectDB = require('./config/db');
+// const cors = require('cors');
+// const dotenv = require('dotenv');
+
+// const authRoutes = require('./routes/auth');
+// const productRoutes = require('./routes/products');
+
+// dotenv.config();
+// connectDB();
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json()); 
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/products', productRoutes);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+// module.exports = app;
+
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
@@ -7,20 +34,20 @@ const authRoutes = require('./routes/auth');
 const productRoutes = require('./routes/products');
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
