60a7dafcc470b1e187c62a82412cd71c58e631dd
eldanii13 <luis.cantilloooo13@gmail.com>
2024-11-09 22:22:47 -0500

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -18,6 +18,11 @@ const productSchema = mongoose.Schema({
     viernes: { type: Number, default: 0 },
     sabado: { type: Number, default: 0 },
   },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',  
+    required: true, 
+  },
 });
 
 const Product = mongoose.model('Product', productSchema);
