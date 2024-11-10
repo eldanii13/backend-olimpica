432a80878a3fcda75a39e5f7c45a0f0f8628ff69
eldanii13 <luis.cantilloooo13@gmail.com>
2024-11-09 22:23:52 -0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,45 +1,49 @@
-const express = require("express");
-const Product = require("../models/Product");
+const express = require('express');
+const authenticateToken = require('../middlewares/auth');
+const Product = require('../models/Product');
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/", authenticateToken, async (req, res) => {
   const { sap, name, days } = req.body;
-
+  
   try {
-    const product = await Product.create({ sap, name, days });
+    const product = await Product.create({
+      sap, 
+      name, 
+      days,
+      userId: req.user._id  
+    });
     res.status(201).json(product);
   } catch (error) {
     res.status(500).json({ message: "Error en el servidor" });
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/", authenticateToken, async (req, res) => {
   try {
-    const products = await Product.find({});
+    const products = await Product.find({ userId: req.user._id });
     res.json(products);
   } catch (error) {
     res.status(500).json({ message: "Error en el servidor" });
   }
 });
 
-router.put("/:sap", async (req, res) => {
+router.put("/:sap", authenticateToken, async (req, res) => {
   const { sap } = req.params;
   const { days } = req.body;
 
   try {
-    const updatedProduct = await Product.findOneAndUpdate(
-      { sap },
-      { days },
-      { new: true }
-    );
-    if (!updatedProduct) {
-      return res.status(404).send("Producto no encontrado");
+    const product = await Product.findOne({ sap, userId: req.user._id });
+    if (!product) {
+      return res.status(404).send("Producto no encontrado o no pertenece al usuario");
     }
-    res.send(updatedProduct);
+
+    product.days = days;
+    await product.save();
+    res.json(product);
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Error al actualizar el producto");
+    res.status(500).json({ message: "Error al actualizar el producto" });
   }
 });
 
